96d80b0d4c82a4ea6e90def79a9ad69a4559bc21
Loki Harris <22lharris@wakatipu.school.nz>
2025-04-02 15:07:49 +1300

    starting order system

--- assessment.py
+++ assessment.py
@@ -1,35 +1,69 @@
-burgers = ["Mig Bac", "Cheese Burger", "Chicken Burger"]
+burger_list = [
+    ["Mig Bac", 15.99], 
+    ["Cheese Burger", 10.99], 
+    ["Chicken Burger", 10.99]
+]
 
+cart_items = []
 
 
 def menu():
+    """
+    is the menu where users will chooses what they want to do.
+    """
+
     while True:
+
         user_input = input("Where would you like to go?\n1: Order a burger\n"
         "2: Cart\n3: checkout\n4: exit\n> ")
+
         if user_input == "1":
             order()
             break
+
         elif user_input == "2":
             cart()
             break
+
         elif user_input == "3":
             checkout()
             break
+
         elif user_input == "4":
             end()
+
         else:
-            print("That is not an option")
+            print("That is not an option\nPlease select a number from 1-4")
 
 
 def checkout():
     print("Hello")
-    
+
+
 def order():
-    print(burgers)
+    print(burger_list)
+    while True:
+        found_burger = find_burger()
+        print(found_burger)
+
+
+
+def find_burger():
+    while True:
+        user_input = input("Please enter the name of the burger you would like \n> ").lower().strip()
+        for burger in burger_list:
+            if burger[0].strip().lower() == user_input:
+                burger_found = burger
+                return burger_found
+            
+
+        print("Burger not found")
+
 
 
 def cart():
     print("Hor")
+
 
 def end():
     print("See you next time.")
