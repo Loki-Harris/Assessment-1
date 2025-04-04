7a3bb431ffeb85a964e6decb96401b8d329bab68
Loki Harris <22lharris@wakatipu.school.nz>
2025-04-04 13:24:14 +1300

    order system done I think

--- assessment.py
+++ assessment.py
@@ -1,7 +1,8 @@
 burger_list = [
-    ["Mig Bac", 15.99], 
-    ["Cheese Burger", 10.99], 
-    ["Chicken Burger", 10.99]
+    [1, "Mig Bac", 16], 
+    [2, "Cheese Burger", 10], 
+    [3, "Chicken Burger", 11],
+    [4, "Quater Pounder", 9],
 ]
 
 cart_items = []
@@ -41,28 +42,69 @@ def checkout():
 
 
 def order():
-    print(burger_list)
     while True:
-        found_burger = find_burger()
-        print(found_burger)
+        print(burger_list)
+        user_input = input("Please enter the number of the burger you would like\n> ")
+        if user_input == "1":
+            cart_items.append(burger_list[0])
+            print("You have added a Mig Bac to your cart")
+            fries()
+            again()
+            break
+        elif user_input == "2":
+            cart_items.append(burger_list[1])
+            print("You have added a Cheese Burger to you cart")
+            fries()
+            again()
+            break
+        elif user_input == "3":
+            cart_items.append(burger_list[2])
+            print("You have added a Chicken Burger to you cart")
+            fries()
+            again()
+            break
+        elif user_input == "4":
+            cart_items.append(burger_list[3])
+            print("You have added a Quater Pounder to you cart")
+            fries()
+            again()
+            break
+        else:
+            print("That is not an option")
 
-
-
-def find_burger():
+def fries():    
+    """
+    This asks users if they would like fries with their burger
+    """
     while True:
-        user_input = input("Please enter the name of the burger you would like \n> ").lower().strip()
-        for burger in burger_list:
-            if burger[0].strip().lower() == user_input:
-                burger_found = burger
-                return burger_found
-            
+        fries = input("Would you like fries with your burger? (Y/N)\n> ")
+        if fries == "Y" or fries == "y":
+            print("Alright fries have been added to cart")
+            cart_items.append("Fries")
+            break
+        elif fries == "N" or fries == "n":
+            print("Alright")
+            menu()
+        else:
+            print("please enter 'Y' or 'N'")
 
-        print("Burger not found")
-
-
+def again():
+    """
+    This is used to ask the user if they would like to order another burger
+    """
+    while True:
+        again = input("Would you like to order another burger? (Y/N)\n> ")
+        if again == "Y" or again == "y":
+            print("Alright")
+            order()
+        elif again == "N" or again == "n":
+            print("Alright")
+            menu()
+        else:
+            ("Please enter 'Y' or 'N'")
 
 def cart():
-    print("Hor")
+    print(cart_items)
 
 
 def end():
